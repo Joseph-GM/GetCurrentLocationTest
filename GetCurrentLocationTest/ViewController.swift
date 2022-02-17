953b477536c51c33d83b32376a66eef0903a993f
Joseph-GM <jkim.english75@gmail.com>
2022-02-17 14:34:09 +0900

    adding change coordinate print

--- GetCurrentLocationTest/ViewController.swift
+++ GetCurrentLocationTest/ViewController.swift
@@ -12,6 +12,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     var locationManager: CLLocationManager!
     
+    var currentLatitudeString: String = "37.5125226"
+    var currentLongitudeString: String = "126.7122712"
+    var currentLatitudeNumber: Double = 37.5125226 {
+        didSet {
+            currentLatitudeString = String(currentLatitudeNumber)
+        }
+    }
+    var currentLongitudeNumber: Double = 126.7122712 {
+        didSet {
+            currentLongitudeString = String(currentLongitudeNumber)
+        }
+    }
+    var currentLocationCoordinate: CLLocationCoordinate2D  {
+        get{
+            return CLLocationCoordinate2D(latitude: currentLatitudeNumber, longitude: currentLongitudeNumber)
+        }
+        set {
+            currentLatitudeNumber = newValue.latitude
+            currentLongitudeNumber = newValue.longitude
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,10 +45,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
         
+        print("init coordinate all")
+        print("currentLocationCoordinate : \(currentLocationCoordinate)")
+        print("currentLatitudeNumber : \(currentLatitudeNumber)")
+        print("currentLongitudeNumber : \(currentLongitudeNumber)")
+        print("currentLatitudeString : \(currentLatitudeString)")
+        print("currentLongitudeString : \(currentLongitudeString)")
+        
+        
         let coor = locationManager.location?.coordinate
+        if let newlatitude = coor?.latitude, let newlongitude = coor?.longitude {
+            currentLocationCoordinate = CLLocationCoordinate2D(latitude: newlatitude, longitude: newlongitude)
+        }
+        print("data from location Manager : \(coor?.latitude), \(coor?.longitude)")
         
-        print("\(coor?.latitude), \(coor?.longitude)")
-        
+        print("after coordinate all")
+        print("currentLocationCoordinate : \(currentLocationCoordinate)")
+        print("currentLatitudeNumber : \(currentLatitudeNumber)")
+        print("currentLongitudeNumber : \(currentLongitudeNumber)")
+        print("currentLatitudeString : \(currentLatitudeString)")
+        print("currentLongitudeString : \(currentLongitudeString)")
     }
 
 
